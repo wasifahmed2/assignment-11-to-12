dd8a13e2a3aee8ca5671360ce4f422963b16758b
wasif <ahmedwasif036@gmail.com>
2024-09-17 00:49:44 +0500

    first commit

---
+++ app.js
@@ -0,0 +1,60 @@
+/// CHAPTER 11 TO 12 /////
+
+//Q no : 01 
+//?
+
+//Q no : 02
+// var num1 = prompt("enter your number")
+// var num2 = prompt("enter your number")
+// var num = +(num1, num2)
+// if (num1 > num2) {
+//     alert(num1 + " is large " + num2)
+// } else if (num2 > num1) {
+//     alert(num2 + " is large " + num1)
+// } else (
+//     alert(num1 + "  are equal number  " + num2)
+// 
+
+//Q no : 03
+// var user_inp = prompt("Enter a number")
+// var num = parseInt(user_inp)
+// if(user_inp > 0){
+// alert("its a positive number")
+// }else if(user_inp < 0){
+// alert("its a negative number")
+// }else{
+//     alert(" not a number")
+// }
+
+//Q no : 04
+// var user = prompt("Enter a letter")
+// var vowel_array = ["A","a", "E","e","I","i","O","u"]
+// var concat = user.includes(vowel_array)
+// if(user == vowel_array){
+//     alert("it is vowel")
+// }else{
+//     alert("not a vowel")
+// }
+
+//Q no : 05
+// var correctPassword = '123123'
+// var userPassword = prompt('Please enter your password:')
+// if (userPassword === "") {
+//     alert('Please enter your password');
+// } else if (userPassword === correctPassword) {
+//     alert('Correct! The password you entered matches the original password.');
+// } else {
+//     alert('Incorrect password.');
+// }
+
+//Q no : 06
+// var hour = +prompt('enter a hour');
+// if (hour < 18) {
+//     alert("good day")
+// }else{
+//     alert("Good evening")
+// }
+
+//Q no : 07
+///???
+
